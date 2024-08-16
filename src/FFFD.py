c3d1eaebfd2c4af66bccfe72c20925fc60092e4c
terrapin@loggingTurtle-Vl <campbellmath001@gmail.com>
2024-08-15 21:28:23 -0700

    modify directory structure /remove hmtl write

--- src/FFFD.py
+++ src/FFFD.py
@@ -1,9 +1,7 @@
-
-
 import sys
 from datetime import datetime, timedelta
 
-logfile = f"/root/FFFDlogs/FFFD_log_run_{datetime.today().strftime('%Y-%m-%d-%H-%M-%S')}"
+logfile = f"/home/terrapin/FFFDlogs/FFFD_log_run_{datetime.today().strftime('%Y-%m-%d-%H-%M-%S')}"
 
 import logging
 logger = logging.getLogger(__name__)
@@ -13,13 +11,12 @@ logging.basicConfig(filename = logfile, level = logging.DEBUG)
 import pandas as pd
 
 yesterday = (datetime.today() - timedelta(days = 1)).strftime('%Y-%m-%d')
-csv_filename = f"/root/FFFD_Data/FFFD_{yesterday}.csv"
+csv_filename = f"/home/terrapin/FFFD_Data/FFFD_{yesterday}.csv"
 
 logger.debug(f'Beginning download of data for {yesterday}')
 try:
     tabs = pd.read_html('https://eservices.fairfield.ca.gov/FireLog/')
     tabs[1].to_csv(csv_filename)
-    tabs[1].to_html(f'/var/www/html/FFFD/FFFD_{yesterday}.html')
     logger.info(f'The script has executed Successfully for Firelog {yesterday}')
 except Exception as e:
     logger.error(f'an error has occured when fetching the table: {e}')
